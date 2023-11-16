bfa7c77f6da0269bfb934ac15fa547935b56c98b
ayonshafiul <ayonshafiul@gmail.com>
2023-11-16 23:23:29 +0600

    feat: add blog and docs

---
+++ blog/2023-10-01-peyara-remote-mouse-and-released.md
@@ -0,0 +1,59 @@
+---
+slug: peyara-remote-mouse-and-released
+title: Peyara Remote Mouse and Keyboard Released
+tags: [wifi remote, remote mouse]
+authors: ayonshafiul
+---
+
+# Hello
+
+Introducing "Peyara Remote Mouse and Keyboard," an open-source cross-platform Server and Client App pair that simplifies the way you interact with your desktop. Enjoy seamless control over your computer's mouse and keyboard functions right from your mobile device, all while experiencing the convenience of zero ads and cross-platform compatibility.
+
+## Desktop Server
+
+Peyara Remote Desktop Server, available for Windows, macOS, and Linux is a Desktop client that turns your dekstop into an remote server for allowing Mouse and Keyboard controls.
+
+## Mobile Client
+
+The Peyara Remote Mouse and Keyboard mobile client app, available for Android (and soon to be availale on iOS) connects with the desktop client by QRCode and provides touchpad, keyboard, and media controls.
+
+### Connectivity Features
+
+#####
+
+- Connect using WIFI
+- Easy and effortless QRCode scanning
+- Automatic server detection
+- Fast device switching
+
+### Touchpad Features
+
+##### Experience a true touchpad feel with intuitive gestures:
+
+- Single Tap
+- Double Tap
+- Right Click on Two Finger Tap
+- Two Finger Scroll gesture
+- Three Finger pan gesture to move
+
+### Keyboard Features
+
+##### Utilize the virtual keyboard to input basic text with ease.
+
+- Input text using mobile keyboard
+- Capital and lowercase letters supported
+- Enter and Backspace key supported
+- (Advanced keys coming soo)
+
+### Media Features
+
+##### Take command of your media playback with convenient controls.
+
+- Control media volume
+- Play, pause, stop, skip to the previous or next track
+
+### Open Source and Zero Ads
+
+Peyara Remote Mouse and Keyboard is driven by an open-source ethos, inviting the community to contribute and enhance the software. Enjoy an ad-free experience, focusing on the functionality that matters most to you.
+
+[Get Started](https://peyara-remote-mouse.vercel.app) by downloading both desktop server and the mobile client.
---
+++ docusaurus.config.js
@@ -0,0 +1,127 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Peyara Remote Mouse and Keyboard",
+  tagline:
+    "An open source, zero ads, cross platform mobile client providing touchpad, keyboard and media controls for wirelessly controlling desktop  connected to the same wifi.",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: "./sidebars.js",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+
+        sitemap: {
+          changefreq: "weekly",
+          priority: 0.5,
+          ignorePatterns: ["/tags/**"],
+          filename: "sitemap.xml",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "Peyara Remote Mouse and Keyboard",
+        logo: {
+          alt: "Peyara Remote Mouse and Keyboard",
+          src: "img/logo.png",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Docs",
+            to: "/docs/get-started",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/ayonshafiul/peyara-mouse-server",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Docs",
+                to: "/docs/download",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Made with ❤️ in Bangladesh. Site Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
